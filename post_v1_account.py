afc052835a00eae0fab1a5c3d399afa4f42dcbc0
Veronica Miller <85158018+i464@users.noreply.github.com>
2023-06-23 00:37:50 +0200

    created a user registration method call

--- post_v1_account.py
+++ post_v1_account.py
@@ -1,20 +1,31 @@
 import requests
 import json
 
-url = "http://localhost:5051/v1/account"
+def post_v1_account():
+    """
+    Register new user
+    :return:
+    """
+    url = "http://localhost:5051/v1/account"
 
-payload = json.dumps({
-  "login": "login44",
-  "email": "test@test44.com",
-  "password": "login44"
-})
-headers = {
-  'X-Dm-Auth-Token': '<string>',
-  'X-Dm-Bb-Render-Mode': '<string>',
-  'Content-Type': 'application/json',
-  'Accept': 'text/plain'
-}
+    payload = {
+        "login": "login44",
+        "email": "test@test44.com",
+        "password": "login44"
+    }
+    headers = {
+        'X-Dm-Auth-Token': '<string>',
+        'X-Dm-Bb-Render-Mode': '<string>',
+        'Content-Type': 'application/json',
+        'Accept': 'text/plain'
+    }
 
-response = requests.request("POST", url, headers=headers, data=payload)
+    response = requests.request(
+        method="POST",
+        url=url,
+        headers=headers,
+        json=payload
+    )
 
-print(response.text)
+response = post_v1_account()
+print(response)
